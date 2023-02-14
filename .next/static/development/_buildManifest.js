b28669f9b8edd9f2bc6aee26460ba280143cf468
bennisan86 <info@bennisan.be>
2023-02-14 16:48:07 +0100

    syncerooni

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/howitworks":["static\u002Fchunks\u002Fpages\u002Fhowitworks.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhowitworks"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
