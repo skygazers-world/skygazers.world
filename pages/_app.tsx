e0b48771d6d294f66d1eb290432567e420164771
bennisan86 <info@bennisan.be>
2023-05-05 11:52:56 +0200

    G analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/shared/Layout'
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<Layout>
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+  };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+  };
+  }, [router.events]);
+  return (
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BDRE17X6PJ', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
+      
+  <Layout>
   <Component {...pageProps} />
-  </Layout>)
+  </Layout>
+  </>)
 }
